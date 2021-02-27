10f56054a4f5c68c770b1db70db6cd6f4d6ea389
Yang Wang <yang.wang.dec@gmail.com>
2021-02-27 13:15:58 -0500

    working on debugview stuff

---
+++ Assets/Scripts/RebootController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RebootController : MonoBehaviour
+{
+    public int minuteTillReboot = 2;
+    public bool active = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Cycle());
+    }
+
+    IEnumerator Cycle()
+    {
+        while (active)
+        {
+            yield return new WaitForSeconds(minuteTillReboot * 60);
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
